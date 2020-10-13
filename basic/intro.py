b1c7ec522bffc20ca4b9df8f1527f37b2578bd7e
Zemian Deng <zemian.deng@oracle.com>
2020-10-13 09:30:27 -0400

    Add

---
+++ basic/intro.py
@@ -0,0 +1,12 @@
+# Quick Tour of Python 3
+
+# String
+s1 = "Hello"
+s2 = "World"
+print(s1)
+print(s1 + s2)
+print(s1 + " " + s2)
+print(f"{s1} {s2}")
+print("{} {}".format(s1, s2))
+# old and not recommended usage
+print("%s %s" % (s1, s2))
