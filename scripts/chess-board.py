37a9b79a8aa6a5da95d5b8d1d2d01eed332325ee
Zemian Deng <zemiandeng@gmail.com>
2022-03-17 20:09:10 -0400

    Use row and col vars

--- scripts/chess-board.py
+++ scripts/chess-board.py
@@ -1,6 +1,7 @@
-size = 8
-for x in range(size):
-    for y in range(size):
+# print a chess-board grid that has alternate color squares
+row, col = 8, 8
+for x in range(row):
+    for y in range(col):
         color = "X" if (x + y) % 2 == 0 else "O"
         print(color, end=' ')
     print()
