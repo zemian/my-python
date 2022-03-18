9f6cc323565a9b9afd3530d1939270eddfc57a10
Zemian Deng <zemiandeng@gmail.com>
2022-03-18 11:14:30 -0400

    Parse locale alias

--- scripts/list-locales.py
+++ scripts/list-locales.py
@@ -2,8 +2,46 @@
 # https://stackoverflow.com/questions/53320311/how-do-i-find-all-available-locales-in-python
 #
 import locale
-locales = list(locale.locale_alias.keys())
+from collections import defaultdict
+
+#locale._print_locale()
+# not very useful info.
+
+# Example value: zh_TW.big5 ir tt_RU.UTF-8@iqtelif
+locales = list(locale.locale_alias.values())
 locales.sort()
-for locale in locales:
+summary_counts = defaultdict(set)
+for locale_str in locales:
+    #print(locale_str)
+
+    lang_country, *encoding_list = locale_str.split('.', 2)
+    lang, *country_list = lang_country.split('_', 2)
+
+    # Skip lang that does not have two chars
+    if len(lang) < 2:
+        continue
+
+    locale = {'lang': lang}
+    summary_counts['lang'].add(lang)
+
+    if len(country_list) > 0:
+        locale['country'] = country_list[0]
+        summary_counts['country'].add(locale['country'])
+
+    if len(encoding_list) > 0:
+        encoding, *script_list = encoding_list[0].split('@', 2)
+        locale['encoding'] = encoding
+        summary_counts['encoding'].add(locale['encoding'])
+
+        if len(script_list) > 0:
+            locale['script'] = script_list[0]
+            summary_counts['script'].add(locale['script'])
+
     print(locale)
-print(f"Total {len(locales)} locales found.")
+
+print("===Summary counts")
+print(f"Language count: {len(summary_counts['lang'])}")
+print(f"Country count: {len(summary_counts['country'])}")
+print(f"Encoding count: {len(summary_counts['encoding'])}")
+print(f"Script count: {len(summary_counts['script'])}")
+
